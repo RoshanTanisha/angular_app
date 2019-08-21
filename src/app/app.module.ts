b253aaead6ca12ff9c7646887bcd126a90bd7bb3
RoshanTanisha <t.bhayani@yahoo.com>
2019-08-21 08:52:05 +0530

    removed IMessage interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {IMessage} from './IMessage';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
